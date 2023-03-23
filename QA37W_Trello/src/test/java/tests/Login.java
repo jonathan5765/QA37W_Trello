c5d6a20673e7aa9d39017487643c66de43317b77
jonathan5765 <jonathan5765@gmail.com>
2023-03-23 11:14:36 +0200

    login

--- QA37W_Trello/src/test/java/tests/Login.java
+++ QA37W_Trello/src/test/java/tests/Login.java
@@ -1,10 +1,18 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Login extends TestBase{
+public class Login extends TestBase {
     @Test
-    public void Login1(){
+    public void Login1() {
+        app.getUser().initLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillINLoginForm("jonathan5765@gmail.com", "Zeylik2504");
+        app.getUser().submitLogin();
+        app.getUser().pause(2000);
+
+        Assert.assertTrue(app.getUser().isLogged());
 
     }
 }
